6f3cf360f8a93bc625e6d5cb8e3b798c5635c2fb
Joe Brew <joebrew@gmail.com>
2021-07-12 14:39:11 -0500

    read in from aws

---
+++ 00_read_data.R
@@ -0,0 +1,65 @@
+library(aws.s3)
+library(readr)
+library(dplyr)
+
+# Read in credentials
+creds <- read_csv('credentials/aws.csv')
+
+Sys.setenv(
+  "AWS_ACCESS_KEY_ID" = creds$`Access key ID`,
+  "AWS_SECRET_ACCESS_KEY" = creds$`Secret access key`,
+  "AWS_DEFAULT_REGION" = "us-east-2"
+)
+
+# Get bucket details
+bucket <- get_bucket(bucket = 'sewaneedata',
+                     max = Inf)
+
+# Loop through every element of the bucket and extract information
+# in a systematic fashion
+n_objects <- length(bucket)
+results_list <- list()
+for(i in 1:n_objects){
+  message(i)
+  # Get the individual object
+  this_photo <- bucket[[i]]
+  # save certain attributes
+  photo_df <- tibble(key = this_photo$Key,
+                     size = this_photo$Size)
+  # Deposit the results into the results_list
+  results_list[[i]] <- photo_df
+}
+# Combine all the objects in results_list into one dataframe
+results <- bind_rows(results_list)
+
+# Save a csv of the results as our photo index
+write_csv(results, 'photo_index.csv')
+
+# Now that we have an index of all the photos in the bucket
+# We want to SAVE those photos locally on the hard drive
+if(!dir.exists('images')){
+  message('creating an images folder in ', getwd())
+  dir.create('images')
+} else {
+  message('images folder already exists. not creating one.')
+}
+
+# Using the "results" data frame so as to get file info
+for(i in 1:nrow(results)){
+  message(i , ' of ', nrow(results))
+  # capture the row 
+  this_row <- results[i,]
+  # define the local path
+  local_path <- file.path('images', this_row$key)
+  # see if the file exists already or not
+  already_got_it <- file.exists(local_path)
+  if(already_got_it){
+    message('---', this_row$key, ' already downloaded. Skipping.')
+  } else {
+    message('---Going to download ', this_row$key)
+    save_object(object = this_row$key,
+               bucket = bucket,
+               file = local_path)
+  }
+  
+}
