30066017d04844491254e846d8aa7983f6e1eb0f
dirkkayitare <86245252+dirkkayitare@users.noreply.github.com>
2021-07-16 13:14:31 -0500

    Update examples_platypus.R

--- examples_platypus.R
+++ examples_platypus.R
@@ -27,7 +27,7 @@ relative_paths <- list.files('images',
                              all.files = TRUE,
                              full.names = TRUE,
                              recursive = TRUE,
-pattern = 'jpg')
+                             pattern = 'jpg')
 
 # # Comment out the below (once you have confirmed it works)
 # relative_paths <- c(
@@ -46,6 +46,11 @@ label_dir <- 'images_labelled'
 if(!dir.exists(label_dir)){
   dir.create(label_dir)
 }
+predictions_dir <- 'predictions'
+if(!dir.exists(predictions_dir)){
+  dir.create(predictions_dir)
+}
+
 
 # Create a list for saving results
 results_list <- list()
@@ -55,58 +60,74 @@ counter <- 0
 for(i in 1:length(test_img_paths)){
   message('Plotting ', i, ' of ', length(test_img_paths))
   
+  
   # Get the specific path
   test_img_path <- test_img_paths[i]
-  test_img <- test_img_path %>%
-    map(~ {
-      image_load(., target_size = c(416, 416), grayscale = FALSE) %>%
-        image_to_array() %>%
-        `/`(255)
-    }) %>%
-    abind(along = 4) %>%
-    aperm(c(4, 1:3))
-  test_preds <- test_yolo %>% predict(test_img)
   
-  # # Take a peak at the predictions
-  # str(test_preds)
+  # Get the relative path
+  relative_path <- relative_paths[i]
   
-  try({
-    test_boxes <- get_boxes(
-      preds = test_preds, # Raw predictions form YOLOv3 model
-      anchors = coco_anchors, # Anchor boxes
-      labels = coco_labels, # Class labels
-      obj_threshold = 0.5, # Object threshold
-      nms = TRUE, # Should non-max suppression be applied
-      nms_threshold = 0.6, # Non-max suppression threshold
-      correct_hw = FALSE # Should height and width of bounding boxes be corrected to image height and width
-    )
-    # Save the results in the results list
-    these_results <- test_boxes[[1]]
-    out <- bind_cols(
-      these_results,
-      tibble(path = relative_paths[i])
-    )
-    # 
+  # Transform the relative path into a file name which is compatible
+  # with data (instead of photo)
+  relative_data_path <- file.path(predictions_dir, paste0(unlist(lapply(strsplit(relative_path, split = '.', fixed = TRUE), function(x){x[1]})), '.RData'))
+  relative_data_path <- gsub('/', '&&&', relative_data_path)
+  relative_data_path <- gsub('predictions&&&', 'predictions/', relative_data_path)
+  already_exists <- file.exists(relative_data_path)
+  
+  if(already_exists){
+    load(relative_data_path)
+  } else {
+    # predictions do not exist for this file, we need to run from scratch
     
-    plot_boxes(
-      images_paths = test_img_path, # Images paths
-      boxes = test_boxes,#list(test_boxes), # Bounding boxes
-      correct_hw = TRUE, # Should height and width of bounding boxes be corrected to image height and width
-      labels = coco_labels, # Class labels
-      save_dir = label_dir,
-      plot_images = TRUE
-    )
+    test_img <- test_img_path %>%
+      map(~ {
+        image_load(., target_size = c(416, 416), grayscale = FALSE) %>%
+          image_to_array() %>%
+          `/`(255)
+      }) %>%
+      abind(along = 4) %>%
+      aperm(c(4, 1:3))
+    test_preds <- test_yolo %>% predict(test_img)
+    
+    # # Take a peak at the predictions
+    # str(test_preds)
+    
+    try({
+      test_boxes <- get_boxes(
+        preds = test_preds, # Raw predictions form YOLOv3 model
+        anchors = coco_anchors, # Anchor boxes
+        labels = coco_labels, # Class labels
+        obj_threshold = 0.6, # Object threshold
+        nms = TRUE, # Should non-max suppression be applied
+        nms_threshold = 0.6, # Non-max suppression threshold
+        correct_hw = FALSE # Should height and width of bounding boxes be corrected to image height and width
+      )
+      # Save the results in the results list
+      these_results <- test_boxes[[1]]
+      out <- bind_cols(
+        these_results,
+        tibble(path = relative_paths[i])
+      )
+      # save the prediction
+      save(out, file = relative_data_path)
+      
+      plot_boxes(
+        images_paths = test_img_path, # Images paths
+        boxes = test_boxes,#list(test_boxes), # Bounding boxes
+        correct_hw = TRUE, # Should height and width of bounding boxes be corrected to image height and width
+        labels = coco_labels, # Class labels
+        save_dir = label_dir,
+        plot_images = TRUE
+      )  
+    })
+    
     counter <- counter + 1
     results_list[[counter]] <- out
-  })
-  
-
+    
+  }
 }
-results <- bind_rows(results_list)
-
-
-
-
-
-# Got here? Great. Look here for how to do this on your own custom data:
-# https://github.com/maju116/platypus#yolov3-object-detection-with-custom-dataset
+  results <- bind_rows(results_list)
+  
+  
+  # Got here? Great. Look here for how to do this on your own custom data:
+  # https://github.com/maju116/platypus#yolov3-object-detection-with-custom-dataset
